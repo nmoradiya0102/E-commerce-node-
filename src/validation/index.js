ed985ac635a83cb90ce55a350d0d573882b240bd
Niyati Moradiya <115994850+nmoradiya0102@users.noreply.github.com>
2023-09-17 22:18:15 +0530

    Delete src/validation directory

--- src/validation/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.categoryValidation = require("./category.validation");
-module.exports.productValidation = require("./product.validation");
-module.exports.userValidation = require("./user.validation");
-module.exports.sub_categoryValidation = require("./sub.category.validation");
-module.exports.cartValidation = require("./cart.validation");
-module.exports.tokenValidation = require("./token.validation");
-module.exports.orderValidation = require("./order.validation");
